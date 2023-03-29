28085faa50520c5615daf53dafac39028b9c48da
Joose Ahonen <t0ahjo00@students.oamk.fi>
2023-03-29 19:35:32 +0300

    Vizualisation4

--- webapp/src/components/Visu4.js
+++ webapp/src/components/Visu4.js
@@ -6,7 +6,7 @@ export default function Visu4() {
   const [v4nationalstate, setV4National] = useState(null);
   return (
     <div>      
-      <DataImport setData={setV4National}  path="7/V4_National_CO2_emissions" />
+      <DataImport setData={setV4National}  path="7/V4_National_CO2_emissions/Finland" />
       <Graph v4nationalstate={v4nationalstate}/>
     </div>
   );
@@ -23,43 +23,37 @@ export function Visu4Information() {
 function Graph(v4nationalstate) {
   const dat = Object.keys(v4nationalstate);
   
-    const data = {
-        datasets: [{
-            label: "Data" ,
-            data: dat.map((v, i) => ({ x: v, y: Object.values(v4nationalstate)[i] })),
-            borderColor: "rgba(3, 64, 120, 1)",
-            backgroundColor: "rgba(3, 64, 120, 0.5)",
-            pointRadius: 1,
-            tension: 0.4,
-            yAxisID: "y"
-        },],
-    };
-    const options = {
-      responsive: true,
-      plugins: {
-        legend: {
-          position: "top",
-        },
+  const data = {
+      datasets: [{
+          label: "Finland" ,
+          data: dat.map((v, i) => ({ x: v, y: Object.values(v4nationalstate)[i] })),
+          borderColor: "rgba(3, 64, 120, 1)",
+          backgroundColor: "rgba(3, 64, 120, 0.5)",
+          pointRadius: 1,
+          tension: 0.4,
+          yAxisID: "y"
+      },],
+  };
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
       },
-      scales: {
-        x: {
-          type: "linear",
-          ticks: {
-            stepSize: 1000
-          }
+    },
+    scales: {
+      x: {
+        type: "time",
+        time: {
+            unit: dat,
         },
-        y: {
-          beginAtZero: true,
-          type: "linear",
-          position: "left"
-        },
-        y2:  {
-          beginAtZero: true,
-          type: "linear",
-          position: "right"
-        }
-      },
-    };
+    },
+    yAxis: {
+        type: "linear",
+    },
+    },
+  };
+
     return (
         <div>
             <h1> CO2 emissions by country </h1>
