d3cee6bdc157798c11d7ae2177dea8fe05509254
Tuskajussi <jussi.tug@gmail.com>
2023-03-30 19:27:50 +0300

    Confirm dialogi toimii

--- webapp/src/components/ConfirmationDialog.js
+++ webapp/src/components/ConfirmationDialog.js
@@ -3,10 +3,12 @@ import React from "react";
 function ConfirmationDialog({yesFunction, noFunction }) {
 
     return (
-        <div className="dialog">
-            <h3>Are you sure?</h3>
-            <button className="small-btn" onClick={yesFunction}>Yes</button>
-            <button className="small-btn" onClick={noFunction}>No</button>
+        <div className="dialog-wrapper">
+            <div className="dialog">
+                <h3>Are you sure?</h3>
+                <button className="small-btn" onClick={yesFunction}>Yes</button>
+                <button className="small-btn" onClick={noFunction}>No</button>
+            </div>
         </div>
     );
 }
