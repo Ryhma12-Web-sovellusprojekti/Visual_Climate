e4ca4c0af413adfaff4a2b552a594eeb5eb6195c
justiina <112779447+justiina@users.noreply.github.com>
2023-03-31 12:13:10 +0300

    Update View2.js

--- webapp/src/pages/View2.js
+++ webapp/src/pages/View2.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Visu4, { Visu4Information } from "../components/Visu4";
-import Visu5, { Visu5Information } from "../components/Visu5";
 
 function View2() {
   return <div>
