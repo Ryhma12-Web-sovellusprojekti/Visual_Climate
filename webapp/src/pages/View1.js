65b404c8157fc97b8e3039cde9ef8fd792a412eb
Essi Nissinen <t1nies@students.oamk.figit config --global user.email t1nies@students.oamk.fi>
2023-03-31 20:28:40 +0300

    V2 added

--- webapp/src/pages/View1.js
+++ webapp/src/pages/View1.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Visu1,{Visu1Information} from "../components/Visu1";
+import Visu2 from "../components/Visu2";
 import Visu3, { Visu3Information } from "../components/Visu3";
 
 function View1( {goBack}) {
     return <div>
         <Visu1 goBack={goBack}/>
         <Visu1Information />
+        <Visu2/>
         <Visu3 />
         <Visu3Information />
      </div>
