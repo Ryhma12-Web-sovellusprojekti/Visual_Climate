11c0ce4d3c146fe65d63fe0748545d8ead0e41ea
justiina <112779447+justiina@users.noreply.github.com>
2023-03-31 12:07:42 +0300

    visualization 5 done

--- webapp/src/pages/View2.js
+++ webapp/src/pages/View2.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Visu4, { Visu4Information } from "../components/Visu4";
+import Visu5, { Visu5Information } from "../components/Visu5";
 
 function View2() {
   return <div>
       <Visu4 />
       <Visu4Information />
+      <Visu5 />
+      <Visu5Information />
     </div>
 }
 
