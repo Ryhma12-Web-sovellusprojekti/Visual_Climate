3b32ae8f74e19b481db4b5c2be8b6b2025a57d22
Joose Ahonen <t0ahjo00@students.oamk.fi>
2023-04-03 19:13:35 +0300

    korjauksia

--- webapp/src/components/Visu4.js
+++ webapp/src/components/Visu4.js
@@ -4,10 +4,13 @@ import { Line } from "react-chartjs-2";
 
 export default function Visu4() {
   const [v4nationalstate, setV4National] = useState(null);
-  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedCountries, setSelectedCountries] = useState([]);
 
   const handleInputChange = (event) => {
-    setSelectedCountry(event.target.value);
+    const selectedOptions = Array.from(event.target.selectedOptions).map(
+      (option) => option.value
+    );
+    setSelectedCountries(selectedOptions);
   };
 
   const path = `7/V4_National_CO2_emissions`;
@@ -18,59 +21,44 @@ export default function Visu4() {
 
   let countryData = null;
   if (v4nationalstate) {
-    countryData = v4nationalstate[selectedCountry];
+    countryData = selectedCountries.reduce((data, country) => {
+      data[country] = v4nationalstate[country];
+      return data;
+    }, {});
   }
-
   return (
     <div>
-      <input
-        type="text"
-        value={selectedCountry}
+      <select
+        value={selectedCountries}
         onChange={handleInputChange}
-        list="countryList"
-      />
-      <datalist id="countryList">
+        multiple
+      >
         {v4nationalstate &&
           Object.keys(v4nationalstate).map((country) => (
-            <option key={country} value={country} />
+            <option key={country} value={country}>
+              {country}
+            </option>
           ))}
-      </datalist>
+      </select>
       <DataImport setData={setV4National} path={path} />
       {countryData && <Graph countryData={countryData} />}
     </div>
   );
 }
 
-export function Visu4Information() {
-  return (
-    <div>
-      <a
-        href="https://www.icos-cp.eu/science-and-impact/global-carbon-budget/2021"
-        target="_blank"
-        rel="noreferrer"
-      >
-        National emissions data source
-      </a>
-    </div>
-  );
-}
-
 function Graph({ countryData }) {
   const data = {
-    labels: Object.keys(countryData),
-    datasets: [
-      {
-        label: "CO2 emissions by country",
-        data: Object.values(countryData),
-        borderColor: "rgba(3, 64, 120, 1)",
-        backgroundColor: "rgba(3, 64, 120, 0.5)",
-        pointRadius: 1,
-        tension: 0.4,
-        yAxisID: "y",
-      },
-    ],
+    labels: Object.keys(countryData)[0],
+    datasets: Object.keys(countryData).map((country) => ({
+      label: country,
+      data: Object.values(countryData[country]),
+      borderColor: "rgba(3, 64, 120, 1)",
+      backgroundColor: "rgba(3, 64, 120, 0.5)",
+      pointRadius: 1,
+      tension: 0.4,
+      yAxisID: "y",
+    })),
   };
-
   const options = {
     responsive: true,
     plugins: {
@@ -84,11 +72,25 @@ function Graph({ countryData }) {
       },
     },
   };
-
   return (
     <div className="chart">
       <h1>CO2 emissions by country</h1>
       <Line data={data} options={options} />
+    </div>
+  );
+}
+
+
+export function Visu4Information() {
+  return (
+    <div>
+      <a
+        href="https://www.icos-cp.eu/science-and-impact/global-carbon-budget/2021"
+        target="_blank"
+        rel="noreferrer"
+      >
+        National emissions data source
+      </a>
     </div>
   );
 }
