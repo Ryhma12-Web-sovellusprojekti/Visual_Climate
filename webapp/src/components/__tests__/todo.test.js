a3c79809c90ef4d05ee316c45d00bd7f331c8b50
Tuskajussi <jussi.tug@gmail.com>
2023-04-10 18:14:59 +0300

    snapshots tested

--- webapp/src/components/__tests__/todo.test.js
+++ webapp/src/components/__tests__/todo.test.js
@@ -1,9 +1,21 @@
 import {render, screen, cleanup} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
 import Footer from '../Footer';
 
-test('Footer', () => {
+afterEach(() => {
+    cleanup();
+});
+
+test('Footer manual test', () => {
     render(<Footer/>);
     const footerElement = screen.getByTestId('Footer1');
     expect(footerElement).toBeInTheDocument;
+    expect(footerElement).toHaveTextContent('© Ryhmä 12 2023');
+    expect(footerElement).toContainHTML('<footer data-testid="Footer1">© Ryhmä 12 2023</footer>');
 }); 
 
+test('Matches snapshot', () =>{
+    const tree = renderer.create(<Footer />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
