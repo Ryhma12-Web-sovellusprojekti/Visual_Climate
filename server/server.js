9ae7e3d2c74560338317ee956b57f64ad3fbe87e
Essi Nissinen <t1nies@students.oamk.figit config --global user.email t1nies@students.oamk.fi>
2023-04-18 20:01:19 +0300

    server.js gets customview data

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const { rtdb } = require('./firebase.js')
+const { rtdb, fsdb} = require('./firebase.js')
+
 
 app.use(express.json());
 app.use(express.urlencoded( {extended: true} ));
@@ -15,5 +16,24 @@ app.get("/get/:row/:visu/:table", (req, res) => {
         res.send(snapshot.val());
       });
   });
+
+//get custom view data with document id
+app.get("/get/customview/:id", async (req, res) => {
+  const docRef = await doc(collection(fsdb, "customview"), req.params.id);
+  getDoc(docRef)
+     .then((doc) => {
+         if (doc.exists()) {
+          res.send(doc.data());
+         } else {
+          res.status(404).send("Document not found");
+         }
+     })
+     .catch((error) => {
+      console.error("Error getting document:", error);
+      res.status(500).send("Error getting document");
+     })
+});
+
+
 
 app.listen(port, () => { console.log(`Server started on port ${port}`) })
