683cdf34fbcdbf7e130f9da351f633cb2a30185a
Tuskajussi <jussi.tug@gmail.com>
2023-04-20 18:08:51 +0300

    controllerit toimii ja testattu.

--- server/controllers/visualsController.js
+++ server/controllers/visualsController.js
@@ -16,22 +16,22 @@ const { rtdb } = require('../firebase.js');
       console.error("Error fetching data:", error);
       res.status(500).send("Error fetching data");
     });
-}
+  };
 
   // get data for graphs (visualizations 4-5)
   exports.getVisuals4to5= (req, res) => {
     rtdb.ref(req.params.row+'/'+req.params.visu)
-    .once("value")
-    .then((snapshot) => {
-      const data = snapshot.val();
-      if (!data) {
-        res.status(404).send("Data not found");
-        return;
-      }
-      res.send(data);
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-      res.status(500).send("Error fetching data");
-    });
-  }
+  .once("value")
+  .then((snapshot) => {
+    const data = snapshot.val();
+    if (!data) {
+      res.status(404).send("Data not found");
+      return;
+    }
+    res.send(data);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+    res.status(500).send("Error fetching data");
+  });
+};
--- server/server.js
+++ server/server.js
@@ -5,9 +5,10 @@ const cors = require('cors')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const routes = require('./routes')(app);
+
 app.use(cors({
   origin: 'http://localhost:3000'
 }));
 
+const routes = require('./routes')(app);
 app.listen(port, () => { console.log(`Server started on port ${port}`) })
