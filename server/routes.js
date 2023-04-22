736380d0bf2d38316b93448f2fe61a57b32bd80c
Joose Ahonen <t0ahjo00@students.oamk.fi>
2023-04-22 14:57:00 +0300

    mahdollinen ratkaisu (ei toimi vielä)

--- server/routes.js
+++ server/routes.js
@@ -2,19 +2,31 @@ const visualsController = require("./controllers/visualsController");
 const customViewsController = require("./controllers/customViewsController");
 const userController = require("./controllers/userController");
 
+const verifyToken = (req, res, next) => {
+    const idToken = req.headers.authorization;
+    admin.auth().verifyIdToken(idToken)
+      .then((decodedToken) => {
+        req.user = decodedToken;
+        next();
+      })
+      .catch((error) => {
+        console.error('Error while verifying Firebase ID token:', error);
+        res.status(403).send('Unauthorized');
+      });
+  };
 
 module.exports = function(app) {
-    app.get("/get/visudata/:row/:visu/:table", visualsController.getVisuals1to3);
-    app.get("/get/visudata/:row/:visu", visualsController.getVisuals4to5);
+    app.get("/get/visudata/:row/:visu/:table", verifyToken, visualsController.getVisuals1to3);
+    app.get("/get/visudata/:row/:visu/", verifyToken, visualsController.getVisuals4to5);
 
-    app.get("/get/customview/:id", customViewsController.getCustomView);
-    app.get("/all/customview/:id", customViewsController.getCustomViews);
-    app.post("/create/customview", customViewsController.createCustomView);
-    app.delete("/delete/customview/:id", customViewsController.deleteCustomView);
-    app.delete("/deleteall/customview/:id", customViewsController.deleteAllCustomViews);
+    app.get("/get/customview/:id", verifyToken, customViewsController.getCustomView);
+    app.get("/all/customview/:id", verifyToken, customViewsController.getCustomViews);
+    app.post("/create/customview", verifyToken, customViewsController.createCustomView);
+    app.delete("/delete/customview/:id", verifyToken, customViewsController.deleteCustomView);
+    app.delete("/deleteall/customview/:id", verifyToken, customViewsController.deleteAllCustomViews);
 
-    app.get("/check/:userId", userController.checkUserExists);
-    app.get("/getname/:userId", userController.getDisplayname);
-    app.post('/createuser', userController.createUser);
-    app.delete('/deleteuser/:userId', userController.deleteUser);
+    app.get("/check/:userId", verifyToken, userController.checkUserExists);
+    app.get("/getname/:userId", verifyToken, userController.getDisplayname);
+    app.post('/createuser', verifyToken, userController.createUser);
+    app.delete('/deleteuser/:userId', verifyToken, userController.deleteUser);
 };
