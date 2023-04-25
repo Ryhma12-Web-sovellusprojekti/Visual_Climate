3e546af3390003f17bb411eff5d3ef9d0ebba622
Joose Ahonen <t0ahjo00@students.oamk.fi>
2023-04-25 17:46:32 +0300

    viimeistely

--- client/src/components/SignOutDelete.js
+++ client/src/components/SignOutDelete.js
@@ -19,7 +19,7 @@ export function DeleteSignedUser() {
         headers: {
             Authorization: `Bearer ${token}`,
           },
-    })
+        })
         .then(axios.delete(`${serverUrl}deleteuser/${user.uid}`, {
             headers: {
                 Authorization: `Bearer ${token}`,
