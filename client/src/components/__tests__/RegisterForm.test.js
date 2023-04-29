e3bb801ec544440126d6521ea0e87f4deaa7878c
Joose Ahonen <t0ahjo00@students.oamk.fi>
2023-04-29 11:16:09 +0300

    RegisterForm.test.js kommentoitu ja siivottu

--- client/src/components/__tests__/RegisterForm.test.js
+++ client/src/components/__tests__/RegisterForm.test.js
@@ -6,6 +6,7 @@ import LoginLinks from "../LoginLinks"
 import RegisterForm from '../LoginForms'
 import { auth } from "../../firebase-config";
 
+// Create a jest-mock function for the useNavigate hook
 const mockedUsedNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
@@ -15,20 +16,32 @@ jest.mock('react-router-dom', () => ({
 describe(("RegisterForm tests"), () => {
 
     test("does not show RegisterForm by default", () => {
+
+        // Render the LoginLinks component and get the queryByText function from the result
         const { queryByText } = render(<LoginLinks />);
+
+        // Query the page for the text "Not registered yet? Sign up here!"
         const registerForm = queryByText("Not registered yet? Sign up here!");
 
-        //RegisterForm does not appear when the Sign up button is not clicked
+        // Assert that the registerForm element is not in the document
         expect(registerForm).toBeNull();
     });
-
+    
     test("shows form after Sign Up button is clicked", async () => {
+
+        // Set up the user event library
         const user = userEvent.setup();
+
+        // Render the LoginLinks component and get the getByText and queryByText functions from the result
         const { getByText, queryByText } = render(<LoginLinks />);
+
+        // Get the "Sign Up" button
         const signUpButton = getByText("Sign Up");
 
+        // Simulate a user clicking the "Sign Up" button
         await user.click(signUpButton);
 
+        // Query the page for various elements that should be present in the RegisterForm
         const registerFormText = queryByText("Not registered yet? Sign up here!");
         const firstNamePlacehoder = screen.getByPlaceholderText("First Name...");
         const lastNamePlacehoder = screen.getByPlaceholderText("Last Name...");
@@ -37,7 +50,7 @@ describe(("RegisterForm tests"), () => {
         const passwordConfPlacehoder = screen.getByPlaceholderText("Password confirmation...");
         const submitButton = screen.getByTestId("signup-submit");
 
-        //finds RegisterForm elements when the Sign up button is clicked
+        // Assert that all of the expected elements are in the document
         expect(registerFormText).toBeInTheDocument();
         expect(firstNamePlacehoder).toBeInTheDocument();
         expect(lastNamePlacehoder).toBeInTheDocument();
@@ -49,19 +62,29 @@ describe(("RegisterForm tests"), () => {
 
     test('gives a note every input field where the data is missing', async () => {
 
+        // Set up the user event library
         const user = userEvent.setup();
+
+        // Render the RegisterForm component
         render(<RegisterForm />);
+
+        // Get the "submit" button
         const submitButton = screen.queryByTestId("signup-submit");
-        
+
+        // Simulate a user clicking the "submit" button without entering any data
         await user.click(submitButton);
+
+        // Ensure that mockedUsedNavigate function is not called with /home route
         await expect(mockedUsedNavigate).not.toHaveBeenCalledWith('/home');
+
+        // Find info texts for each input field
         const fnInfo = screen.queryByTestId("firstname-info");
         const lnInfo = screen.queryByTestId("lastname-info");
         const emailInfo = screen.queryByTestId("email-info");
         const passwordInfo = screen.queryByTestId("password-info");
         const passwordConfInfo = screen.queryByTestId("password-confirmation-info");
-        
-        //finds info texts
+
+        // Verify that all the info texts are present in the document
         expect(fnInfo).toBeInTheDocument();
         expect(lnInfo).toBeInTheDocument();
         expect(emailInfo).toBeInTheDocument();
@@ -71,10 +94,16 @@ describe(("RegisterForm tests"), () => {
 
     });
 
-   test('RegisterForm gives a note if the email does not contain @ character', async () => {
+    // Test to verify that a message is displayed if the email does not contain '@' character
+    test('RegisterForm gives a note if the email does not contain @ character', async () => {
 
+        // Set up the user event library
         const user = userEvent.setup();
+
+        // Render the RegisterForm component
         render(<RegisterForm />);
+
+        // Get the input fields
         const firstNamePlacehoder = screen.getByPlaceholderText("First Name...");
         const lastNamePlacehoder = screen.getByPlaceholderText("Last Name...");
         const emailPlacehoder = screen.getByPlaceholderText("Email...");
@@ -82,25 +111,31 @@ describe(("RegisterForm tests"), () => {
         const passwordConfPlacehoder = screen.getByPlaceholderText("Password confirmation...");
         const submitButton = screen.getByTestId("signup-submit");
 
+        // Simulate a user entering data without '@' character in email
         await user.type(firstNamePlacehoder, 'Firstname');
         await user.type(lastNamePlacehoder, 'Lastname');
-        //email does not contain @ character
         await user.type(emailPlacehoder, 'example.fi');
         await user.type(passwordPlacehoder, 'password');
         await user.type(passwordConfPlacehoder, 'password');
         await user.click(submitButton);
-        const emailInfo = screen.getByTestId("email-info");
-        //does not let the user forward
-        await expect(mockedUsedNavigate).not.toHaveBeenCalledWith('/home');
-        //finds emailInfo from the document
-        expect(emailInfo).toBeInTheDocument();
 
+        // Ensure that mockedUsedNavigate function is not called with /home route
+        await expect(mockedUsedNavigate).not.toHaveBeenCalledWith('/home');
+
+        // Find the email info text and verify that it is present in the document
+        const emailInfo = screen.getByTestId("email-info");
+        expect(emailInfo).toBeInTheDocument();
     });
 
     test('gives a note if the email is not in the correct format', async () => {
 
+        // Set up the user event library
         const user = userEvent.setup();
+
+        // Render the RegisterForm component
         render(<RegisterForm />);
+
+        // Get the input fields
         const firstNamePlacehoder = screen.getByPlaceholderText("First Name...");
         const lastNamePlacehoder = screen.getByPlaceholderText("Last Name...");
         const emailPlacehoder = screen.getByPlaceholderText("Email...");
@@ -110,14 +145,17 @@ describe(("RegisterForm tests"), () => {
 
         await user.type(firstNamePlacehoder, 'Firstname');
         await user.type(lastNamePlacehoder, 'Lastname');
+
          //email is not in the correct format
         await user.type(emailPlacehoder, '@example.fi');
         await user.type(passwordPlacehoder, 'password');
         await user.type(passwordConfPlacehoder, 'password');
         await user.click(submitButton);
         const emailInfo = screen.getByTestId("email-info");
+
         //does not let the user forward
         await expect(mockedUsedNavigate).not.toHaveBeenCalledWith('/home');
+
         //finds emailInfo from the document
         expect(emailInfo).toBeInTheDocument();
 
@@ -125,8 +163,13 @@ describe(("RegisterForm tests"), () => {
 
     test('gives a note if password is too short', async () => {
 
+        // Set up the user event library
         const user = userEvent.setup();
+
+        // Render the RegisterForm component
         render(<RegisterForm />);
+
+        // Get the input fields
         const firstNamePlacehoder = screen.getByPlaceholderText("First Name...");
         const lastNamePlacehoder = screen.getByPlaceholderText("Last Name...");
         const emailPlacehoder = screen.getByPlaceholderText("Email...");
@@ -134,24 +177,31 @@ describe(("RegisterForm tests"), () => {
         const passwordConfPlacehoder = screen.getByPlaceholderText("Password confirmation...");
         const submitButton = screen.getByTestId("signup-submit");
 
+        // Simulate a user entering too short password
         await user.type(firstNamePlacehoder, 'Firstname');
         await user.type(lastNamePlacehoder, 'Lastname');
         await user.type(emailPlacehoder, 'name@example.fi');
-        //password is too short, must be at least six characters
         await user.type(passwordPlacehoder, 'pass');
         await user.type(passwordConfPlacehoder, 'pass');
         await user.click(submitButton);
         const passwordInfo = screen.getByTestId("password-info");
+
         //does not let the user forward
         await expect(mockedUsedNavigate).not.toHaveBeenCalledWith('/home');
+
         //finds passwordInfo from the document
         expect(passwordInfo).toBeInTheDocument();
-
     });
 
     test("gives a note if password and its confirmation don't match", async () => {
+
+        // Set up the user event library
         const user = userEvent.setup();
+
+        // Render the RegisterForm component
         render(<RegisterForm />);
+
+        // Get the input fields
         const firstNamePlacehoder = screen.getByPlaceholderText("First Name...");
         const lastNamePlacehoder = screen.getByPlaceholderText("Last Name...");
         const emailPlacehoder = screen.getByPlaceholderText("Email...");
@@ -159,22 +209,22 @@ describe(("RegisterForm tests"), () => {
         const passwordConfPlacehoder = screen.getByPlaceholderText("Password confirmation...");
         const submitButton = screen.getByTestId("signup-submit");
 
+        // Simulate a user entering wrong password
         await user.type(firstNamePlacehoder, 'Firstname');
         await user.type(lastNamePlacehoder, 'Lastname');
         await user.type(emailPlacehoder, 'name@example.fi');
-        //password and confirmation are different
         await user.type(passwordPlacehoder, 'password');
         await user.type(passwordConfPlacehoder, 'passwodr');
         await user.click(submitButton);
         const passwordConfInfo = screen.getByTestId("password-confirmation-info"); 
-        //does not let the user forward
-        expect(mockedUsedNavigate).not.toHaveBeenCalledWith('/home');
-        //finds password confimration info from the document
-        expect(passwordConfInfo).toBeInTheDocument();
 
+         //does not let the user forward
+        expect(mockedUsedNavigate).not.toHaveBeenCalledWith('/home');
+
+        //finds passwordConfInfo from the document
+        expect(passwordConfInfo).toBeInTheDocument();
     });
 
-    //a test email is created to test successful registration
     function generateTestEmailAddress() {
         const now = new Date();
         const day = String(now.getDate()).padStart(2, '0');
@@ -189,9 +239,13 @@ describe(("RegisterForm tests"), () => {
       const testAddress = generateTestEmailAddress();
     test('successful sign up when form receives correct information', async () => {
 
+        // Set up the user event library
         const user = userEvent.setup();
 
+        // Render the RegisterForm component
         render(<RegisterForm />);
+
+        // Get the input fields
         const firstNamePlacehoder = screen.getByPlaceholderText("First Name...");
         const lastNamePlacehoder = screen.getByPlaceholderText("Last Name...");
         const emailPlacehoder = screen.getByPlaceholderText("Email...");
@@ -199,17 +253,17 @@ describe(("RegisterForm tests"), () => {
         const passwordConfPlacehoder = screen.getByPlaceholderText("Password confirmation...");
         const submitButton = screen.getByTestId("signup-submit");
 
+        // Simulate a user entering correct credentials
         await user.type(firstNamePlacehoder, 'Firstname');
         await user.type(lastNamePlacehoder, 'Lastname');
         await user.type(emailPlacehoder, testAddress);
         await user.type(passwordPlacehoder, 'password555');
         await user.type(passwordConfPlacehoder, 'password555');
-
         await user.click(submitButton);
 
-        //wait a little while so the user's data will be saved to the database
         await new Promise(resolve => setTimeout(resolve, 1500));
-        //user is redirected to home when the registration is successful
+
+        //allows the user to navigate to the home page
         await expect(mockedUsedNavigate).toHaveBeenCalledWith('/home');
     });
     const deleteTestUser = async () => {
@@ -218,7 +272,6 @@ describe(("RegisterForm tests"), () => {
           await user.delete(); 
         }
       };
-      //delete tests user after tests are done
       afterAll(async () => {
         await deleteTestUser();
       });
