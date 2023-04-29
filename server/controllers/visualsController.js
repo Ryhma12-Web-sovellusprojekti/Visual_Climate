92cd1011f1464f21017401846e9bb47a1e3ba560
Essi Nissinen <essieveliina.nissinen@gmail.com>
2023-04-29 20:57:09 +0300

    comments to visualsController

--- server/controllers/visualsController.js
+++ server/controllers/visualsController.js
@@ -2,36 +2,46 @@ const { rtdb } = require('../firebase.js');
 
 // get data for graphs (visualizations 1-3)
 exports.getVisuals1to3 = (req, res) => {
-  rtdb.ref(req.params.row+'/'+req.params.visu+'/'+req.params.table)
+  // the visualization data is retrieved from the path obtained as request parameters from Firebase realtime database
+  rtdb.ref(req.params.row + '/' + req.params.visu + '/' + req.params.table)
     .once("value")
     .then((snapshot) => {
+      // if data is found, it's saved to data variable
       const data = snapshot.val();
       if (!data) {
+        // if data is not found, give response "Data not found"
         res.status(404).send("Data not found");
         return;
       }
+      // give data as a response
       res.send(data);
     })
     .catch((error) => {
       console.error("Error fetching data:", error);
+      // if there is some other error, give info "Error fetching data"
       res.status(500).send("Error fetching data");
     });
-  };
+};
 
 // get data for graphs (visualizations 4-5)
-exports.getVisuals4to5= (req, res) => {
-  rtdb.ref(req.params.row+'/'+req.params.visu)
+exports.getVisuals4to5 = (req, res) => {
+  // the visualization data is retrieved from the path obtained as request parameters from Firebase realtime database
+  rtdb.ref(req.params.row + '/' + req.params.visu)
     .once("value")
     .then((snapshot) => {
+      // if data is found, it's saved to data variable
       const data = snapshot.val();
       if (!data) {
-      res.status(404).send("Data not found");
-      return;
+        // if data is not found, give response "Data not found"
+        res.status(404).send("Data not found");
+        return;
       }
+      // give data as a response
       res.send(data);
-  })
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-    res.status(500).send("Error fetching data");
-  });
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+      // if there is some other error, give info "Error fetching data"
+      res.status(500).send("Error fetching data");
+    });
 };
