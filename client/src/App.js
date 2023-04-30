f9c07b645f1703a0cf93914f83609f04b0e7e400
Joose Ahonen <t0ahjo00@students.oamk.fi>
2023-04-30 10:52:22 +0300

    kommentteja ja siivoomista

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,21 @@ import Home from "./pages/Home";
 import ShowCustomView from "./pages/ShowCustomView";
 
 function App() {
- 
-  return <div className="App">
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home route={0} />} />
-        <Route path="/home/View1" element={<Home route={"View1"}/>} />
-        <Route path="/home/View2" element={<Home route={"View2"}/>} />
-        <Route path="/home/CustomView" element={<Home route={"CustomView"}/>} />
-        <Route path="/CustomView/:id" element={<ShowCustomView />} />
-        <Route path="/home/CustomViewEdit" element={<Home route={"CustomViewEdit"}/>} />
-      </Routes>
-    </Router>
-  </div>;
+
+    // Returns routes
+    return <div className="App">
+            <Router>
+                <Routes>
+                      <Route path="/" element={<Login />} />
+                      <Route path="/home" element={<Home route={0} />} />
+                      <Route path="/home/View1" element={<Home route={"View1"}/>} />
+                      <Route path="/home/View2" element={<Home route={"View2"}/>} />
+                      <Route path="/home/CustomView" element={<Home route={"CustomView"}/>} />
+                      <Route path="/CustomView/:id" element={<ShowCustomView />} />
+                      <Route path="/home/CustomViewEdit" element={<Home route={"CustomViewEdit"}/>} />
+                </Routes>
+            </Router>
+        </div>;
 }
 
 export default App;
